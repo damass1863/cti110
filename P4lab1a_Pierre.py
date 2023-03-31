1fea566ecd7589c21fc4f08970f6be545b0c94f5
damass1863 <124935752+damass1863@users.noreply.github.com>
2023-03-30 21:33:22 -0400

    Add files via upload
    p4LAB1a_Draw_Square_triangle

---
+++ P4lab1a_Pierre.py
@@ -0,0 +1,21 @@
+import turtle
+
+# draw a square
+turtle.forward(150)  
+turtle.left(90)  
+turtle.forward(150)  
+turtle.left(90)  
+turtle.forward(150)  
+turtle.left(90)  
+turtle.forward(150)  
+turtle.left(90)
+
+# draw a triangle
+
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.exitonclick()
+
